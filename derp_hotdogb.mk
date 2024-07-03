af003f8ea918db52f85d9de0a2c9f64ee8d07dfb
Matheus <mattvieira123@gmail.com>
2024-07-03 08:37:39 -0300

    hotdogb: derpify

--- derp_hotdogb.mk
+++ derp_hotdogb.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from hotdogb device
 $(call inherit-product, device/oneplus/hotdogb/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Derp stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_hotdogb
+PRODUCT_NAME := derp_hotdogb
 PRODUCT_DEVICE := hotdogb
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := HD1901
@@ -32,3 +32,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 BUILD_FINGERPRINT := OnePlus/OnePlus7T/OnePlus7T:12/SKQ1.211113.001/Q.202303230244:user/release-keys
+
+# Official-ify
+DERP_BUILDTYPE := Official
+EXTRA_UDFPS_ICONS := true

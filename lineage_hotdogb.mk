ef11679fbdb730627d9aaa78a0b2091b86c20c3c
Matheus <mattvieira123@gmail.com>
2024-11-01 11:40:18 -0300

    hotdogb: include RisingOS flags
    Change-Id: Ib173ff557613a17840cfe6b7b51b57a7748a9749

--- lineage_hotdogb.mk
+++ lineage_hotdogb.mk
@@ -14,6 +14,17 @@ $(call inherit-product, device/oneplus/hotdogb/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# RisingOS flags
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+PRODUCT_NO_CAMERA := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon 855+" \
+    RisingMaintainer="Matheus Vieira(matt0301)"
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_hotdogb
 PRODUCT_DEVICE := hotdogb
